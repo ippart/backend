9933f9c86ae6aea8a442f62b672c5dbb80cc213f
Dmitry <irvis@imega.ru>
2017-05-20 13:37:20 +0300

    get categories

--- controller/common/home.php
+++ controller/common/home.php
@@ -19,6 +19,41 @@ class ControllerCommonHome extends \iMega\Controller
         $data['footer']         = $this->load->controller('common/footer');
         $data['header']         = $this->load->controller('common/header');
 
+        $this->getLoader()->model(\iMega\Model\Catalog::CATEGORY);
+        $this->getLoader()->model(\iMega\Model\Catalog::PRODUCT);
+
+        $categories = (new \ModelCatalogCategory())->getCategories(0);
+        $data['categories'] = [];
+
+        foreach ($categories as $category) {
+            if ($category['top']) {
+                // Level 2
+                $children_data = [];
+                $children = (new \ModelCatalogCategory())->getCategories($category['category_id']);
+
+                foreach ($children as $child) {
+                    $filter_data = [
+                        'filter_category_id'  => $child['category_id'],
+                        'filter_sub_category' => true,
+                    ];
+
+                    $children_data[] = [
+                        'name'  => $child['name'] . ($this->getConfig()->get('config_product_count') ? ' (' . (new \ModelCatalogProduct())->getTotalProducts($filter_data) . ')' : ''),
+                        'href'  => $this->getUrl()->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+                    ];
+                }
+
+                // Level 1
+                $data['categories'][] = [
+                    'name'     => $category['name'],
+                    'children' => $children_data,
+                    'column'   => $category['column'] ? $category['column'] : 1,
+                    'href'     => $this->getUrl()->link('product/category', 'path=' . $category['category_id'])
+                ];
+            }
+        }
+
+
         $this->response->setOutput($this->load->view('common/home', $data));
     }
 }
---
+++ src/iMega/Model/Catalog.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace iMega\Model;
+
+final class Catalog
+{
+    const CATEGORY = 'catalog/category';
+    const PRODUCT = 'catalog/product';
+}
